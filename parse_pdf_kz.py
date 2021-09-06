b668f58be81b0693b5f0778966e22f4873fc2a32
Sanzhar <48170101+SanzharMrz@users.noreply.github.com>
2021-09-06 23:25:22 +0600

    Create parse_pdf_kz.py

---
+++ parse_pdf_kz.py
@@ -0,0 +1,120 @@
+import os
+import re
+import json
+import argparse
+import warnings
+
+import torch
+from espnet2.bin.tts_inference import Text2Speech
+from parallel_wavegan.utils import download_pretrained_model
+from parallel_wavegan.utils import load_model, read_hdf5
+from scipy.io.wavfile import write
+
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer
+
+
+class Config:
+    file_to_lang = {'SCANNED - 4108_18411976.pdf': 'kz',
+                    'SCANNED - 4110_353652508.pdf': 'kz',
+                    'Optimzed - tanirbergenova_a.a.berikbaeva_m.a._inzhenerlіk_mehanika_-_2__334400008.pdf_enc6407430.pdf': 'kz',
+                    'Optimzed - sotsialnoe_gosudarstvo_teorija_metodologija_mehanizmy_93821869.pdf_enc9546740.pdf': 'ru',
+                    'Optimzed - book_w3vlpeg_235482945.pdf': 'ru',
+                    'Optimzed - book_664181550_138791945.pdf': 'en',
+                    'Optimzed - 5_999694706.pdf': 'ru',
+                    'Optimzed - 104_860776228.pdf': 'ru',
+                    'Not Optimized - filipov_etnichekie_protsessy_v_rossijskom_megapolise_i_juzhno__kazahstanskom_regio_PAApkoj.pdf': 'ru',
+                    'Not Optimized - book_762283876.pdf_enc4442172.pdf': 'kz',
+                    'Not Optimized - book_717820121_346006649.pdf': 'en',
+                    'Not Optimized - book_246240674.pdf_enc3941923.pdf': 'kz'}
+    start = 1
+    end = 3
+    skip_pages = 2
+    max_len = 140
+    fs = 22050
+    
+
+def get_args():
+    parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--filepath", default='PDF_Examples/', help="path to some pdf files")
+    args = parser.parse_args()
+    return args
+
+
+def parse_text(text):
+    parsed_text = re.sub('([^А-Яа-яa-zA-ZӘәҒғҚқҢңӨөҰұҮүІі-]|[^ ]*[*][^ ]*)', ' ', text).strip()[:Config.max_len]
+    return parsed_text
+
+
+def parse(args):
+    final_json = {}
+    files = filter(lambda x: 'ipynb' not in x, os.listdir(args.filepath))
+
+    for filename in files:
+        head, tail = os.path.split(filename)
+        folder_name, _ = tail.split('.')
+        book_json = {}
+        if not os.path.exists(folder_name):
+            os.mkdir(folder_name)
+
+        for page_num, page_layout in enumerate(extract_pages(filename, password="")):
+            if page_num < Config.start:
+                continue
+            if page_num > Config.end:
+                break
+            if page_num >= Config.skip_pages:
+                
+                parsing_results = []                
+                for element in page_layout:
+                    if isinstance(element, LTTextContainer):
+                        coords = '_'.join(list(map(lambda x: str(round(x)), element.bbox)))
+                        response_filename = f'page_{page_num}_coords_{coords}.wav'
+                        text = parse_text(element.get_text())
+                        if len(text) > 3:
+                            response_json = {
+                                "bbox": element.bbox,
+                                "text": text,
+                                "filename": response_filename
+                            }
+                            with torch.no_grad():
+                                _, c_mel, *_ = text2speech(text.lower())
+                            wav = vocoder.inference(c_mel)
+                            parsing_results.append(response_json)
+                            save_path = os.path.join(folder_name, response_filename)
+                            write(save_path, Config.fs, wav.view(-1).detach().cpu().numpy())
+                book_json[page_num] = parsing_results
+        final_json[tail] = book_json
+    json_filename = os.path.join(folder_name, f'{folder_name}.json')
+    
+    with open(json_filename, 'w') as f:
+        json.dump(final_json, f)
+
+
+if __name__ == '__main__':
+    warnings.simplefilter('ignore')
+    ## specify the path to vocoder's checkpoint
+    vocoder_checkpoint="/home/s.murzakhmetov/audio/tts/espnet/exp/vocoder/checkpoint-400000steps.pkl"
+    vocoder = load_model(vocoder_checkpoint).to("cpu").eval()
+    vocoder.remove_weight_norm()
+
+    ## specify path to the main model(transformer/tacotron2/fastspeech) and its config file
+    config_file = "/home/s.murzakhmetov/audio/tts/espnet/exp/tts_train_raw_char/config.yaml"
+    model_path  = "/home/s.murzakhmetov/audio/tts/espnet/exp/tts_train_raw_char/train.loss.ave_5best.pth"
+
+    text2speech = Text2Speech(
+                        config_file,
+                        model_path,
+                        device="cpu",
+                        # Only for Tacotron 2
+                        threshold=0.5,
+                        minlenratio=0.0,
+                        maxlenratio=10.0,
+                        use_att_constraint=True,
+                        backward_window=1,
+                        forward_window=3,
+                        # Only for FastSpeech & FastSpeech2
+                        speed_control_alpha=1.0,
+                        )
+    text2speech.spc2wav = None
+    args = get_args()
+    parse(args)

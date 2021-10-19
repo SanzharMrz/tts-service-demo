5a056114c8e9a54fc86d458382aba8f46d039188
Sanzhar <48170101+SanzharMrz@users.noreply.github.com>
2021-10-19 22:29:13 +0600

    Update parse_pdf.py

--- parse_pdf.py
+++ parse_pdf.py
@@ -11,18 +11,20 @@ from pdfminer.layout import LTTextContainer
 
 
 class Config:
-    file_to_lang = {'SCANNED - 4108_18411976.pdf': 'kz',
-                    'SCANNED - 4110_353652508.pdf': 'kz',
-                    'Optimzed - tanirbergenova_a.a.berikbaeva_m.a._inzhenerlіk_mehanika_-_2__334400008.pdf_enc6407430.pdf': 'kz',
-                    'Optimzed - sotsialnoe_gosudarstvo_teorija_metodologija_mehanizmy_93821869.pdf_enc9546740.pdf': 'ru',
-                    'Optimzed - book_w3vlpeg_235482945.pdf': 'ru',
-                    'Optimzed - book_664181550_138791945.pdf': 'en',
-                    'Optimzed - 5_999694706.pdf': 'ru',
-                    'Optimzed - 104_860776228.pdf': 'ru',
-                    'Not Optimized - filipov_etnichekie_protsessy_v_rossijskom_megapolise_i_juzhno__kazahstanskom_regio_PAApkoj.pdf': 'ru',
-                    'Not Optimized - book_762283876.pdf_enc4442172.pdf': 'kz',
-                    'Not Optimized - book_717820121_346006649.pdf': 'en',
-                    'Not Optimized - book_246240674.pdf_enc3941923.pdf': 'kz'}
+    file_to_lang = {
+        'SCANNED - 4108_18411976.pdf': 'kz',
+        'SCANNED - 4110_353652508.pdf': 'kz',
+        'Optimzed - tanirbergenova_a.a.berikbaeva_m.a._inzhenerlіk_mehanika_-_2__334400008.pdf_enc6407430.pdf': 'kz',
+        'Optimzed - sotsialnoe_gosudarstvo_teorija_metodologija_mehanizmy_93821869_pdf_enc9546740.pdf': 'ru',
+        'Optimzed - book_w3vlpeg_235482945.pdf': 'ru',
+        'Optimzed - book_664181550_138791945.pdf': 'en',
+        'Optimzed - 5_999694706.pdf': 'ru',
+        'Optimzed - 104_860776228.pdf': 'ru',
+        'Not Optimized - filipov_etnichekie_protsessy_v_rossijskom_megapolise_i_juzhno__kazahstanskom_regio_PAApkoj.pdf': 'ru',
+        'Not Optimized - book_762283876.pdf_enc4442172.pdf': 'kz',
+        'Not Optimized - book_717820121_346006649.pdf': 'en',
+        'Not Optimized - book_246240674.pdf_enc3941923.pdf': 'kz'
+    }
     lang_to_sub = {'en': '[^A-Za-z]+',
                    'ru': '[^А-Яа-я]+'}
     speaker = {'ru': 'aidar_v2',
@@ -30,13 +32,24 @@ class Config:
     sample_rate = 16_000
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     start = 1
-    end = 10
+    end = 100
     skip_pages = 2
-    max_len = 140
+    max_len = 130
+
+    
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+        
+
+def parse_text(text, language):
+    parsed_text = re.sub(Config.lang_to_sub.get(language), ' ', text).strip()[:Config.max_len]
+    return parsed_text
 
 
 def get_funcs(language):
     speaker = Config.speaker.get(language)
+    print(speaker, language)
     model = None
     if language == 'en':
         model, symbols, _, _, apply_tts = torch.hub.load(repo_or_dir='snakers4/silero-models',
@@ -67,17 +80,12 @@ def get_audios(model, language, texts):
                                  sample_rate=Config.sample_rate)[0] for text in texts]
     return audios
 
-
-def parse_text(text, language):
-    parsed_text = re.sub(Config.lang_to_sub.get(language), ' ', text).strip()[:Config.max_len]
-    return parsed_text
-
-
 def parse(args):
     final_json = {}
     files = filter(lambda x: 'ipynb' not in x, os.listdir(args.filepath))
 
     for filename in files:
+        filename = "PDF_Examples/Optimzed - book_664181550_138791945.pdf"
         head, tail = os.path.split(filename)
         folder_name, _ = tail.split('.')
         language = Config.file_to_lang.get(tail)
@@ -93,34 +101,52 @@ def parse(args):
             if page_num > Config.end:
                 break
             if page_num >= Config.skip_pages:
-                
-                parsing_results = []
-                texts = []
-                
+
+                #collect raw text
+                raw_page_text = []
                 for element in page_layout:
                     if isinstance(element, LTTextContainer):
-                        coords = '_'.join(list(map(lambda x: str(round(x)), element.bbox)))
-                        response_filename = f'page_{page_num}_coords_{coords}.wav'
-                        text = parse_text(element.get_text(), language)
-                        if len(text) > 3:
-                            texts.append(text)
-                            response_json = {
-                                "bbox": element.bbox,
-                                "text": text,
-                                "filename": response_filename
-                            }
-                            
-                            parsing_results.append(response_json)
-                audios = get_audios(model, language, texts)
-                
+                        raw_text = element.get_text()
+                        clear_text = parse_text(raw_text, language)
+                        if len(clear_text) > 10:
+                            raw_page_text.append(raw_text)
+
+                # cut it to sentences
+                tok_lang = 'russian' if language=='ru' else 'english'
+                sentences = list(filter(lambda sent: len(sent) > 10, sent_tokenize(' '.join(raw_page_text).replace('\n', ''), language=tok_lang')))
+
+                # clear cutted senteces
+                clear_sentences = list(map(lambda t: parse_text(t, language), sentences))
+
+                # generate audios
+                audios = []
+                for chunk in chunks(clear_sentences, 5):
+                    audios.extend(get_audios(model, language, chunk))
+
+                # create response jsons
+                parsing_results = []
+                for sent_id, sent in enumerate(clear_sentences):
+                    response_filename = f'page_{page_num}_sentid_{sent_id}.wav'
+                    response_json = {
+                        "page": page_num,
+                        "sent_id": sent_id,
+                        "text": sent,
+                        "filename": response_filename
+                    }
+                    parsing_results.append(response_json)
+
+                # create audio wav files
                 for audio, json_ in zip(audios, parsing_results):
                     save_path = os.path.join(folder_name, json_['filename'])
                     torchaudio.save(save_path, audio.unsqueeze(0), Config.sample_rate)
-                    
+
                 book_json[page_num] = parsing_results
+
         final_json[tail] = book_json
+        #TODO remove this break for parsing whole folder
+        break
+
     json_filename = os.path.join(folder_name, f'{folder_name}.json')
-    
     with open(json_filename, 'w') as f:
         json.dump(final_json, f)
 

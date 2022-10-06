3a5532d9e750af080fd03cdf1fa4ccd7f897ea35
KindYAK <yakunin.k@mail.ru>
2022-10-06 14:55:59 +0600

    feat - split text taking sentences borders into consideration

--- configs.py
+++ configs.py
@@ -2,14 +2,18 @@ class Config_kz:
     max_len = 140
     fs = 22050
     config_file = "/opt/espnet/egs2/Kazakh_TTS/tts1/exp/tts_train_raw_char/config.yaml"
-    model_path =  "/opt/espnet/egs2/Kazakh_TTS/tts1/exp/tts_train_raw_char/train.loss.ave_5best.pth"
+    model_path = "/opt/espnet/egs2/Kazakh_TTS/tts1/exp/tts_train_raw_char/train.loss.ave_5best.pth"
     vocoder_checkpoint = "/opt/espnet/egs2/Kazakh_TTS/tts1/exp/vocoder/checkpoint-400000steps.pkl"
 
 
 class Config_ru_en:
-    lang_to_sub = {'en': '[^A-Za-z]+',
-                   'ru': '[^А-Яа-я]+'}
-    speaker = {'ru': 'aidar_v2',
-               'en': 'lj_16khz'}
+    lang_to_sub = {
+        'en': '[^A-Za-zА-Яа-я?!.]+',
+        'ru': '[^А-Яа-яA-Za-z?!.]+'
+    }
+    speaker = {
+        'ru': 'aidar_v2',
+        'en': 'lj_16khz'
+    }
     sample_rate = 16_000
     max_len = 130
--- util.py
+++ util.py
@@ -1,34 +1,45 @@
 import re
+from nltk.tokenize import word_tokenize, sent_tokenize
+
 from configs import Config_ru_en
-from nltk.tokenize import word_tokenize
+
 
 def parse_text(text, language):
     parsed_text = re.sub(Config_ru_en.lang_to_sub.get(language), ' ', text).strip()
     return parsed_text
 
+
 def parse_text_kz(text):
-    parsed_text = re.sub('([^А-Яа-яa-zA-ZӘәҒғҚқҢңӨөҰұҮүІі-]|[^ ]*[*][^ ]*)', ' ', text).strip()
+    parsed_text = re.sub('([^А-Яа-яa-zA-ZӘәҒғҚқҢңӨөҰұҮүІі!?.-]|[^ ]*[*][^ ]*)', ' ', text).strip()
     return parsed_text
+
 
 def chunks_f(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
 
+def r(t):
+    return t.replace(".", "").replace("?", "").replace("!", "").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ")
+
+
 def custom_sent_tokenize(text, language, max_len):
-    sents = []
-    chunk = ''
-    words = word_tokenize(text, language=language)[::-1]
-    while len(words):
-        
-        word = words.pop()
+    sents = sent_tokenize(text)
+    res = []
+    for sent in sents:
+        chunk = ''
+        words = word_tokenize(sent, language=language)[::-1]
+        words = [r(w).strip() for w in words if r(w).strip()]
+        while len(words):
+            word = words.pop()
 
-        if len(chunk + ' ' + word) > max_len:
-            sents.append(chunk.strip())
-            chunk = ''
+            if len(chunk + ' ' + word) > max_len:
+                res.append(chunk.strip())
+                chunk = ''
 
-        chunk += ' '
-        chunk += word
-        
-    sents.append(chunk)
-    return sents
+            chunk += ' '
+            chunk += word
+
+        res.append(chunk.strip())
+
+    return res
